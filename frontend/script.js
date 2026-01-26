f18220d5a1d77c09132c081a35b423f10907e708
cauabackend <cauabackend@gmail.com>
2026-01-26 12:36:06 -0300

    frontend

--- frontend/script.js
+++ frontend/script.js
@@ -1,6 +1,5 @@
 const API_URL = 'https://portfolio-backend-nkp4.onrender.com/api';
 
-
 function toggleTheme() {
     document.body.classList.toggle('dark-theme');
     const btn = document.querySelector('.theme-toggle-btn');
